d8dee316dbaa292d857e2af1b492834c7fffc19b
SamK47 <121657113+SamK47@users.noreply.github.com>
2024-01-18 01:06:13 +0200

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 class E07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which multiplicand multiplication table to show?");
+        System.out.println("Which multiplicand multiplication table to show? ");
         int number = scanner.nextInt();
-        System.out.println("Until which multiplayer?");
+        System.out.println("Until which multiplayer? ");
         int number2 = scanner.nextInt();
         for (int i = 0; i <= number2; i++) {
             System.out.println(i + " x " + number + " = " + (number * i));
