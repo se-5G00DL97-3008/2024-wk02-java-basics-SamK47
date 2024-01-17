d430b343d48395c3c9b134b9e33ad6ded23896a2
SamK47 <121657113+SamK47@users.noreply.github.com>
2024-01-18 01:26:03 +0200

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class E06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which multiplication table to show? ");
+        System.out.println("Which multiplication table to show?");
         int number = scanner.nextInt();
         for (int i = 0; i <= 10; i++) {
             System.out.println(i + " x " + number + " = " + (number * i));
